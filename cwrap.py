62b37010fd28214c7197cae1cda6a47729ba6d3f
Jay Deiman <admin@splitstreams.com>
2012-02-17 13:43:05 -0600

    Added the license notification at the top

--- cwrap.py
+++ cwrap.py
@@ -1,5 +1,20 @@
 #!/usr/local/bin/python
 
+# This file is part of cron-wrap.
+# 
+# cron-wrap is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# cron-wrap is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with cron-wrap.  If not, see <http://www.gnu.org/licenses/>.
+
 from optparse import OptionParser , OptionValueError , OptionGroup
 import cPickle as pickle
 from hashlib import md5
