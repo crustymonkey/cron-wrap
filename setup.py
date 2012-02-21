3af2d1c685646e3e65377e802341d4db6c0f0c76
Jay Deiman <jay@splitstreams.com>
2012-02-21 11:24:14 -0600

    Added a number of things for pypi compatibility

--- setup.py
+++ setup.py
@@ -1,38 +1,25 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from cwrap import __version__ as cwv
-import sys , os , shutil
-
-MANPATHS = (
-    '/usr/man' ,
-    '/usr/share/man' ,
-    '/usr/local/man' ,
-    '/usr/local/share/man'
-)
-
-def installMan(manpage):
-    # Check for the man location
-    for d in MANPATHS:
-        man1 = os.path.join(d , 'man1')
-        if os.path.isdir(d) and os.path.isdir(man1):
-            # We have found a man directory, install!
-            dest = os.path.normpath(os.path.join(man1 , manpage))
-            print 'Copying %s to %s' % (manpage , dest)
-            shutil.copyfile(manpage , dest)
-            os.system('gzip -9 %s' % dest)
 
 setup(name='cron-wrap' ,
-    version=cwv ,
+    version='0.4.2' ,
     author='Jay Deiman' ,
     author_email='admin@splitstreams.com' ,
     url='http://stuffivelearned.org/doku.php?id=programming:python:cwrap' ,
     description='A cron job wrapper used to suppress output' ,
-    scripts=['cwrap.py']
+    long_description='Full documentation can be found in the man page or here: '
+        'http://stuffivelearned.org/doku.php?id=programming:python:cwrap' ,
+    scripts=['cwrap.py'] ,
+    data_files = [ ('man/man1' , ['cwrap.py.1']) ] ,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable' ,
+        'Environment :: Console' ,
+        'Intended Audience :: System Administrators' ,
+        'License :: OSI Approved :: GNU General Public License (GPL)' ,
+        'Natural Language :: English' ,
+        'Operating System :: POSIX' ,
+        'Programming Language :: Python' ,
+        'Topic :: System :: Systems Administration' ,
+    ]
 )
-
-
-
-if 'install' in sys.argv:
-    # Try to install the man page
-    installMan('./cwrap.py.1')
